92d710d8acb2dabf782786a43d214a183359ac05
Agnieszka Sierant <agnieszka@student.agh.edu.pl>
2021-01-17 14:43:02 +0100

    ProductTest is fully working

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -10,24 +10,33 @@ public abstract class Product {
     private final BigDecimal taxPercent;
 
     protected Product(String name, BigDecimal price, BigDecimal tax) {
-        this.name = name;
+       if (name == null || name.equals("")) {
+    	   throw new IllegalArgumentException("You cannot create products with empty or null name");
+       }
+           	this.name = name;
+       
+       if (price == null || price.intValue() < 0) { //moze byc tez tak: (price == null || price.compareTo(new BigDecimal("0")) == -1)
+    	   throw new IllegalArgumentException("You cannot create products with null or negative price");
+       }
+       
         this.price = price;
+       
         this.taxPercent = tax;
     }
 
     public String getName() {
-        return null;
+    	return this.name;
     }
 
     public BigDecimal getPrice() {
-        return null;
+        return this.price;
     }
 
     public BigDecimal getTaxPercent() {
-        return null;
+        return this.taxPercent;
     }
 
     public BigDecimal getPriceWithTax() {
-        return null;
+        return this.price.multiply(this.taxPercent).add(this.price);
     }
 }
